33a585a81a64770f80cf067f1aef86d2dbff7295
michrag <31441542+michrag@users.noreply.github.com>
2025-01-28 10:35:37 +0100

    Hello, YOU

--- 1.HelloWorld/hello_test.go
+++ 1.HelloWorld/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Michele")
+	want := "Hello, Michele"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
